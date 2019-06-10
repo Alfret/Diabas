c8a0dcdaf651ce2c53e13116bd7adfd95c7fbdf9
Christoffer Gustafsson <christoffer.golf@gmail.com>
2019-06-10 11:00:01 +0200

    Test build setup now exists.

---
+++ tests/toml.test.cpp
@@ -0,0 +1,13 @@
+#include "main.test.hpp"
+#include <mods/loader.hpp>
+#include <core/string.hpp>
+
+TEST_SUITE("toml") {
+  TEST_CASE("basic loading") {
+
+    const dib::String core_file_path{ "../mods/core/mod.toml" };
+    auto [result, mod_info] = dib::ModLoader::Load(core_file_path);
+    CHECK(result == dib::ModLoader::LoadResult::kSuccess);
+  }
+
+}
