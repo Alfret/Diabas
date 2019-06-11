1a4af916e444f3a11d1ae17cf5da22443d7f2d0c
Christoffer Gustafsson <christoffer.golf@gmail.com>
2019-06-11 18:15:41 +0200

    Added 'ticked' update, such that the network update will be call at a fixed rate.

--- source/game.cpp
+++ source/game.cpp
@@ -1,4 +1,10 @@
 #include "game.hpp"
+#include <dlog.hpp>
+#include <functional>
+
+// TEMP (for thread sleep to not overwork my linux machine)
+#include <chrono>
+#include <thread>
 
 // ========================================================================== //
 // Game Implementation
@@ -8,14 +14,17 @@ namespace dib
 {
 
 Game::Game(const Descriptor& descriptor)
-  : Application(descriptor)
+    : Application(descriptor)
 {
   GetGraphics().SetClearColor(100 / 255.0f, 149 / 255.0f, 237 / 255.0f, 1.0f);
+  ConnectToServer();
 }
 
 // -------------------------------------------------------------------------- //
 
-Game::~Game() {}
+Game::~Game() {
+  client_.CloseConnection();
+}
 
 // -------------------------------------------------------------------------- //
 
@@ -25,11 +34,29 @@ Game::Update(f64 delta)
   if (IsKeyDown(Key::KEY_ESCAPE)) {
     Exit();
   }
+
+  UpdateNetwork();
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(10));
 }
 
 // -------------------------------------------------------------------------- //
 
 void
 Game::Render() {}
+
+void
+Game::UpdateNetwork() {
+  static const auto callback_fn =
+      std::bind(&Client::Poll, &client_);
+
+  FixedTimeUpdate(kNetTicksPerSec, callback_fn);
+}
+
+void Game::ConnectToServer() {
+  SteamNetworkingIPAddr addr{};
+  addr.SetIPv4(0x7F000001, 24812);
+  client_.Connect(addr);
+}
 
 }
