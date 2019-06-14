8207d238ec3afd1972a5983b2c93341d19dbf576
Alfret <Afisse13@gmail.com>
2019-06-14 09:22:01 +0200

    Added scripts and environment

--- res/demo.js
+++ res/demo.js
@@ -1,3 +1,16 @@
-function init() {
-  return true;
+class CoreMod extends Mod {
+
+    constructor() {
+        super();
+        dlog.info("info: ", 22, "other", true, 3.14, "HELLO");
+    }
+
+    init() {
+        dlog.verbose("CoreMod::init");
+    }
+
+    update(delta) {
+      dlog.verbose("Delta: " + delta);
+    }
+
 }
