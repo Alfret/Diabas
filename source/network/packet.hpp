57b8e26a92ee9ee6befeb61c83fd38cf18b26f67
Christoffer Gustafsson <christoffer.golf@gmail.com>
2019-06-18 11:42:20 +0200

    Better packet type. Now with iterator

--- source/network/packet.hpp
+++ source/network/packet.hpp
@@ -4,14 +4,20 @@
 #include "core/types.hpp"
 #include <vector>
 #include "network/packet_header.hpp"
+#include <alflib/string.hpp>
 
 namespace dib {
+
+// forward declaration
+struct PayloadIterator;
 
 /**
  * A packet consists of a header and a payload, stored in contiguous memory.
  */
 class Packet
 {
+  public:
+  using ValueType = u8;
 
   // ============================================================ //
   // Lifetime
@@ -19,11 +25,27 @@ class Packet
  public:
   Packet();
 
+  /**
+   * Construct a packet with given size.
+   */
   explicit Packet(const std::size_t size);
 
-  Packet(const u8* data, const std::size_t data_count);
+  /**
+   * Copy data into a new packet, starting from header
+   * @param data_count How many bytes to copy over. Must be more than
+   * GetHeaderSize().
+   */
+  Packet(const ValueType* data, const std::size_t data_count);
 
-  Packet(const char* data, const std::size_t data_count);
+  /**
+   * Copy data into payload, will write 0's to header.
+   */
+  Packet(const char8* data, const std::size_t data_count);
+
+  /**
+   * Write string into payload, and write 0's to header
+   */
+  Packet(const alflib::String& string);
 
   // ============================================================ //
   // Packet / General
@@ -31,31 +53,42 @@ class Packet
  public:
 
   /**
-   * Clear the packet.
+   * Clear the packet, will not write anything to the container.
    */
   void Clear();
 
   /**
-   * Get how many bytes are unused in the container.
+   * Get how many bytes are unused in the packet.
    */
-  std::size_t GetBytesLeft() const { return GetCapacity() - size_; }
+  std::size_t GetBytesLeft() const { return GetPacketCapacity() - GetPacketSize(); }
 
   /**
-   * Set amount of bytes allocated for container. Capacity includes both
+   * Set amount of bytes allocated for container. Includes both
    * header and payload.
    */
-  void SetCapacity(const std::size_t capacity);
-
-  std::size_t GetCapacity() const { return container_.size(); }
-
-  std::size_t GetPacketSize() const { return GetHeaderSize() + GetPayloadSize(); }
-
-  const u8* GetPacket() const { return &container_[0]; }
+  void SetPacketCapacity(const std::size_t capacity);
 
   /**
-   * Simply take a data source, and write that into our container.
+   * Get total bytes allocated by packet.
    */
-  bool SetPacket(const u8* data, const std::size_t data_count);
+  std::size_t GetPacketCapacity() const;
+
+  /**
+   * Get total bytes used by packet.
+   */
+  std::size_t GetPacketSize() const;
+
+  const ValueType* GetPacket() const { return &container_[0]; }
+
+ private:
+  ValueType* GetPacket() { return &container_[0]; }
+
+ public:
+  /**
+   * Simply take a data source, and write that into our container.
+   * @return False if data_count is too large for our packet.
+   */
+  bool SetPacket(const ValueType* data, const std::size_t data_count);
 
   // ============================================================ //
   // Header
@@ -67,18 +100,44 @@ public:
 
   const PacketHeader* GetHeader() const;
 
+ private:
+  ValueType* GetHeaderRaw();
+
+ public:
+  /**
+   * Write 0's to header;
+   */
+  void ClearHeader();
+
   // ============================================================ //
   // Payload
   // ============================================================ //
  public:
+  /**
+   * Get used bytes by the payload.
+   */
   std::size_t GetPayloadSize() const;
+
+  /**
+   * Get the total amount of bytes that can be used by the payload.
+   */
+  std::size_t GetPayloadCapacity() const;
 
   /**
    * @return If we could write the entire data into payload.
    */
-  bool SetPayload(const u8* data, const std::size_t data_count);
+  bool SetPayload(const ValueType* data, const std::size_t data_count);
+  bool SetPayload(const char8* str, const std::size_t str_len);
 
-  const u8* GetPayload() const;
+  const ValueType* GetPayload() const;
+
+  ValueType* GetPayload();
+
+  PayloadIterator begin();
+
+  PayloadIterator end();
+
+  alflib::String ToString();
 
   // ============================================================ //
   // Constants
@@ -90,9 +149,11 @@ public:
   // ============================================================ //
   // Member variables
   // ============================================================ //
-  using PacketContainer = std::vector<u8>;
+ private:
+  using PacketContainer = std::vector<ValueType>;
 
   std::size_t size_;
+
   PacketContainer container_{};
 };
 
