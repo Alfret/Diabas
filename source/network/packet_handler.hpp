79e188fe2ef9c208887a8d71e710c647fe622160
Christoffer Gustafsson <christoffer.golf@gmail.com>
2019-06-20 13:06:40 +0200

    Cleaned up and improved the packet handler.

--- source/network/packet_handler.hpp
+++ source/network/packet_handler.hpp
@@ -5,23 +5,43 @@
 #include <functional>
 #include <string_view>
 #include <optional>
+#include <array>
 
 namespace dib
 {
 
+// ============================================================ //
+// Predefined packet types
+// ============================================================ //
+
+enum class PacketHeaderStaticTypes : std::size_t
+{
+  kSync = 0,
+
+  // Must be last, used to count number of constants in the enum
+  kChat
+};
+
+constexpr std::size_t kPacketHeaderStaticTypesCount =
+    static_cast<std::size_t>(PacketHeaderStaticTypes::kChat) + 1;
+
+// ============================================================ //
+// Helper Types
+// ============================================================ //
+
 using PacketHandlerCallback = std::function<void(const Packet&)>;
 
 /**
- * Describes a packet type.
+ * Describes a packet type and how to handle it.
  * PacketHeaderType is stored separately, as key in a map.
-  */
+ */
 struct PacketMeta
 {
   // stored as key in map
   /* PacketHeaderType type; */
 
   // unique name
-  alflib::String name;
+  String name;
 
   // called when this packet type is received.
   PacketHandlerCallback callback;
@@ -33,22 +53,24 @@ struct PacketMeta
 struct PacketMetaSerializable
 {
   PacketHeaderType type;
-  alflib::String name;
+  String name;
 };
 
 // ============================================================ //
+// Packet Handler
+// ============================================================ //
 
+/**
+ * Packet handler maps a packet type with a callback function.
+ */
 class PacketHandler
 {
-   // ============================================================ //
-  // Lifetime
-  // ============================================================ //
  public:
   PacketHandler();
   ~PacketHandler();
 
   // ============================================================ //
-  // Core Methods
+  // Packet Consumer
   // ============================================================ //
  public:
 
@@ -58,23 +80,22 @@ class PacketHandler
   bool HandlePacket(const Packet& packet) const;
 
   /**
-   * Add a packet type with a given name. Suited for dynamically addding
-   * packet types.
-   * @return If we could add the packet type without name collision.
+   *
+   * @param packet_type_name Must be unique.
+   * @return If name is not unique, the add fails and returns false
    */
-  bool AddPacketType(const alflib::String& packet_type_name,
-                     PacketHandlerCallback callback);
+  bool AddDynamicPacketType(const String& packet_type_name,
+                            PacketHandlerCallback callback);
 
   /**
-   * Add a packet type, you decide packet_type and name.
-   * @param type_hint If not unique, it will be modified to be unique.
-   * @return If we could add the packet type without name collision.
+   *
+   * @param packet_type_name Must be unique.
+   * @return If name is not unique, the add fails and returns false
    */
-  bool AddPacketType(PacketHeaderType type_hint,
-                     const alflib::String& packet_type_name,
-                     PacketHandlerCallback callback);
+  bool AddStaticPacketType(const PacketHeaderStaticTypes static_type,
+                           const String& packet_type_name,
+                           PacketHandlerCallback callback);
 
- public:
   enum class SyncResult {
     kSuccess = 0,
 
@@ -90,18 +111,43 @@ class PacketHandler
 
   SyncResult Sync(const std::vector<PacketMetaSerializable>& correct);
 
-  std::vector<PacketMetaSerializable> Serialize() const;
+  // ============================================================ //
+  // Packet Producer
+  // ============================================================ //
+ public:
+  /**
+   * Make a header of static packet type, and fill it in for the packet.
+   */
+  void BuildPacketHeader(Packet& packet,
+                       const PacketHeaderStaticTypes static_type) const;
+
+  /**
+   * Make a header of dynamic packet type, and fill it in for the packet.
+   * @return If we can find a packet type for the given name.
+   */
+  bool BuildPacketHeader(Packet& packet, const String& name) const;
+
+ private:
+  /**
+   * Prepare the packet header.
+   */
+  void BuildPacketHeader(Packet& packet, const PacketHeaderType type) const;
 
   // ============================================================ //
   // Misc
   // ============================================================ //
  public:
+  std::vector<PacketMetaSerializable> Serialize() const;
+
+  /**
+   * Search for the packet type with the given name, among the packet types
+   * that are dynamic.
+   */
+  std::optional<PacketHeaderType> FindDynamicType(const String& name) const;
 
   auto begin() { return packet_metas_.begin(); }
 
   auto end() { return packet_metas_.end(); }
-
-  std::optional<PacketHeaderType> GetType(const alflib::String& string);
 
   std::size_t GetSize() const { return packet_metas_.size(); }
 
@@ -111,8 +157,15 @@ class PacketHandler
   // Member Variables
   // ============================================================ //
 
-private:
+ private:
   std::unordered_map<PacketHeaderType, PacketMeta> packet_metas_;
+
+  /**
+   * The PacketHeaderStaticTypes points to an index in this array.
+   */
+  std::array<PacketHeaderType, kPacketHeaderStaticTypesCount> static_types_{};
+
+  std::unordered_map<String, PacketHeaderType> dynamic_types_{};
 };
 }
 
