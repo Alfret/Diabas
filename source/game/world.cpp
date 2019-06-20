82b47594e2713c03796432d3b413154743ec27cd
Christoffer Gustafsson <christoffer.golf@gmail.com>
2019-06-20 15:41:31 +0200

    Clean world, moved networking stuff into network.

--- source/game/world.cpp
+++ source/game/world.cpp
@@ -1,177 +1,23 @@
 #include "world.hpp"
-#include "core/fixed_time_update.hpp"
-#include <dlog.hpp>
-#include <functional>
-#include <alflib/assert.hpp>
 
 namespace dib {
 
-template<>
 void
-World<Side::kClient>::Update()
+World::Update()
 {
-
-  // update network
-  static bool got_update;
-  auto client = GetClient();
-  static const auto PollClient =
-      std::bind(&Client::Poll, client, std::ref(got_update), std::ref(packet_));
-  got_update = false;
-  FixedTimeUpdate(kNetTicksPerSec, PollClient);
-  if (got_update) {
-    packet_handler_.HandlePacket(packet_);
-  }
+  network_.Update();
 }
 
-template<>
 void
-World<Side::kServer>::Update()
+World::OnCommandNetwork(const std::string_view input)
 {
-
-  // update network
-  static bool got_update;
-  auto server = GetServer();
-  static const auto PollServer =
-      std::bind(&Server::Poll, server, std::ref(got_update), std::ref(packet_));
-  got_update = false;
-  FixedTimeUpdate(kNetTicksPerSec, PollServer);
-  if (got_update) {
-    packet_handler_.HandlePacket(packet_);
-  }
+  network_.NetworkInfo(input);
 }
 
-template<>
 void
-World<Side::kClient>::ConnectToServer()
+World::OnCommandBroadcast(const std::string_view input)
 {
-  SteamNetworkingIPAddr addr{};
-  addr.SetIPv4(0x7F000001, kPort);
-  auto client = GetClient();
-  client->Connect(addr);
+  network_.Broadcast(input);
 }
 
-template<>
-void
-World<Side::kServer>::StartServer()
-{
-  auto server = GetServer();
-  server->StartServer(kPort);
-}
-
-template<>
-void
-World<Side::kClient>::SetupPacketHandler()
-{
-  const auto SyncCb = [](const Packet&) {
-    DLOG_RAW("TODO handle sync packet\n");
-  };
-  bool ok = packet_handler_.AddStaticPacketType(
-    PacketHeaderStaticTypes::kSync, "sync", SyncCb);
-  AlfAssert(ok, "could not add packet type sync");
-
-  const auto ChatCb = [](const Packet& packet) {
-    alflib::String msg = packet.ToString();
-    DLOG_RAW("Server: {}\n", msg);
-  };
-  ok = packet_handler_.AddStaticPacketType(PacketHeaderStaticTypes::kChat, "chat", ChatCb);
-  AlfAssert(ok, "could not add packet type chat");
-
-
-}
-
-template<>
-void
-World<Side::kServer>::SetupPacketHandler()
-{
-  const auto SyncCb = [](const Packet&) {
-    DLOG_RAW("TODO handle sync packet\n");
-  };
-  bool ok = packet_handler_.AddStaticPacketType(
-    PacketHeaderStaticTypes::kSync, "sync", SyncCb);
-  AlfAssert(ok, "could not add packet type sync");
-
-  const auto ChatCb = [](const Packet& packet) {
-    alflib::String msg = packet.ToString();
-    DLOG_RAW("TODO handle chat packets [{}]\n", msg);
-  };
-  ok = packet_handler_.AddStaticPacketType(PacketHeaderStaticTypes::kChat, "chat", ChatCb);
-  AlfAssert(ok, "could not add packet type chat");
-}
-
-template<>
-void
-World<Side::kServer>::NetworkInfo([
-  [maybe_unused]] const std::string_view message) const
-{
-  auto server = GetServer();
-  server->NetworkInfo();
-}
-
-template<>
-void
-World<Side::kClient>::NetworkInfo([
-  [maybe_unused]] const std::string_view message) const
-{}
-
-template<>
-void
-World<Side::kServer>::Broadcast(const std::string_view message) const
-{
-  auto server = GetServer();
-  Packet packet{ message.data(), message.size() };
-  packet_handler_.BuildPacketHeader(packet, PacketHeaderStaticTypes::kChat);
-  server->BroadcastPacket(packet, SendStrategy::kReliable);
-}
-
-template<>
-void
-World<Side::kClient>::Broadcast([
-  [maybe_unused]] const std::string_view message) const
-{}
-
-template<>
-World<Side::kClient>::World()
-  : base_(new Client())
-{
-  SetupPacketHandler();
-  ConnectToServer();
-}
-
-template<>
-World<Side::kServer>::World()
-  : base_(new Server(this))
-{
-  SetupPacketHandler();
-  StartServer();
-}
-
-template<>
-World<Side::kClient>::~World()
-{
-  auto client = GetClient();
-  client->~Client();
-}
-
-template<>
-World<Side::kServer>::~World()
-{
-  auto server = GetServer();
-  server->~Server();
-}
-
-template<>
-NetworkState
-World<Side::kClient>::GetNetworkState() const
-{
-  auto client = GetClient();
-  return client->GetNetworkState();
-}
-
-template<>
-NetworkState
-World<Side::kServer>::GetNetworkState() const
-{
-  auto server = GetServer();
-  return server->GetNetworkState();
-}
 }
