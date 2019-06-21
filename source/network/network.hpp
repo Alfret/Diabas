75f3b3eafc808ceecd08485dbf1b02519cbc27db
Christoffer Gustafsson <christoffer.golf@gmail.com>
2019-06-21 15:32:54 +0200

    Network now exposes a Broadcast function, which broadcast a packet.

--- source/network/network.hpp
+++ source/network/network.hpp
@@ -38,20 +38,28 @@ public:
 public:
   void Update();
 
-  void NetworkInfo(const std::string_view message) const;
-
-  void Broadcast(const std::string_view message) const;
+  /**
+   * Broadcast a packet to all active connections.
+   */
+  void Broadcast(const Packet& packet) const;
 
   void ConnectToServer();
 
   void StartServer();
 
-  void SetupPacketHandler();
+  // ============================================================ //
+  // TMP
+  // ============================================================ //
+  void NetworkInfo(const std::string_view message) const;
+
+  void Broadcast(const std::string_view message) const;
 
   // ============================================================ //
   // Private Methods
   // ============================================================ //
 private:
+  void SetupPacketHandler();
+
   Client* GetClient() const { return static_cast<Client*>(base_); }
   Server* GetServer() const { return static_cast<Server*>(base_); }
 
