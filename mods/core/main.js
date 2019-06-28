e61a0fb5ba7418c594b5cc6078ab61938fdbde3e
Christoffer Gustafsson <christoffer.golf@gmail.com>
2019-06-28 22:42:51 +0200

    Updated to new resources.

--- mods/core/main.js
+++ mods/core/main.js
@@ -12,6 +12,10 @@ export default class CoreMod extends Mod {
     this.tileGrass = new Tile(this, "res/grass_top.png");
     this.tileRock = new Tile(this, "res/rock.png");
     this.tileFurnace = new Tile(this, "res/furnace.png");
+    this.tileFlowerDaisyBlue = new Tile(this, "res/flower_daisy_blue.tga");
+    this.tileFlowerDaisyPurple = new Tile(this, "res/flower_daisy_purple.tga");
+    this.tileFlowerDaisyWhite = new Tile(this, "res/flower_daisy_white.tga");
+    this.tileSunflowerYellow = new Tile(this, "res/sunflower_yellow.tga");
   }
 
   onRegisterTiles(tileManager) {
@@ -21,7 +25,9 @@ export default class CoreMod extends Mod {
     tileManager.registerTile("grass", this.tileGrass);
     tileManager.registerTile("rock", this.tileRock);
     tileManager.registerTile("furnace", this.tileFurnace);
-
+    tileManager.registerTile("flower daisy blue", this.tileFlowerDaisyBlue);
+    tileManager.registerTile("flower daisy purple", this.tileFlowerDaisyPurple);
+    tileManager.registerTile("flower daisy white", this.tileFlowerDaisyWhite);
   }
 
 }
