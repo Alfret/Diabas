c47b682489a93f5713382d9983cb45280cc65515
Christoffer Gustafsson <christoffer.golf@gmail.com>
2019-06-29 10:23:40 +0200

    updated to latest resource files

--- mods/core/main.js
+++ mods/core/main.js
@@ -8,10 +8,13 @@ export default class CoreMod extends Mod {
   }
 
   init() {
-    this.tileDirt = new Tile(this, "res/dirt.png");
-    this.tileGrass = new Tile(this, "res/grass_top.png");
-    this.tileRock = new Tile(this, "res/rock.png");
-    this.tileFurnace = new Tile(this, "res/furnace.png");
+    this.tileDirt = new Tile(this, "res/dirt.tga");
+      this.tileGrass = new Tile(this, "res/grass_top.tga");
+      this.tileGrassTopLeft = new Tile(this, "res/grass_top_left.tga");
+      this.tileGrassTopRight = new Tile(this, "res/grass_top_right.tga");
+      this.tileGrassTopLeftRight = new Tile(this, "res/grass_top_left_right.tga");
+    this.tileRock = new Tile(this, "res/rock.tga");
+    this.tileFurnace = new Tile(this, "res/furnace.tga");
     this.tileFlowerDaisyBlue = new Tile(this, "res/flower_daisy_blue.tga");
     this.tileFlowerDaisyPurple = new Tile(this, "res/flower_daisy_purple.tga");
     this.tileFlowerDaisyWhite = new Tile(this, "res/flower_daisy_white.tga");
@@ -22,12 +25,16 @@ export default class CoreMod extends Mod {
     dlog.verbose("CoreMod::onRegisterTiles()");
 
     tileManager.registerTile("dirt", this.tileDirt);
-    tileManager.registerTile("grass", this.tileGrass);
+      tileManager.registerTile("grass", this.tileGrass);
+      tileManager.registerTile("grass top left", this.tileGrassTopLeft);
+      tileManager.registerTile("grass top right", this.tileGrassTopRight);
+      tileManager.registerTile("grass top left right", this.tileGrassTopLeftRight);
     tileManager.registerTile("rock", this.tileRock);
     tileManager.registerTile("furnace", this.tileFurnace);
     tileManager.registerTile("flower daisy blue", this.tileFlowerDaisyBlue);
     tileManager.registerTile("flower daisy purple", this.tileFlowerDaisyPurple);
-    tileManager.registerTile("flower daisy white", this.tileFlowerDaisyWhite);
+      tileManager.registerTile("flower daisy white", this.tileFlowerDaisyWhite);
+      tileManager.registerTile("sunflower yellow", this.tileSunflowerYellow);
   }
 
 }
