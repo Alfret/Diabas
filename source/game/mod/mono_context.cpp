c46afbe43edcf3284c062542c5f0a2ce097285d1
Filip Björklund <Afisse13@gmail.com>
2019-07-09 11:27:34 +0200

    Temporarily paused modding support

--- source/game/mod/mono_context.cpp
+++ source/game/mod/mono_context.cpp
@@ -1,4 +1,5 @@
 #include "game/mod/mono_context.hpp"
+#include "mono_context.hpp"
 
 // ========================================================================== //
 // Headers
@@ -6,6 +7,7 @@
 
 #include <dlog.hpp>
 #include <mono/metadata/assembly.h>
+#include <mono/metadata/reflection.h>
 
 // ========================================================================== //
 // Private Functions
@@ -40,23 +42,39 @@ MonoContext::MonoContext()
   mCoreImage = mono_assembly_get_image(mCoreAssembly);
   AlfAssert(mCoreImage != nullptr, "Failed get 'diabas.dll' assembly image");
 
-  // Mod
-  mClassMod = mono_class_from_name(mCoreImage, "Diabas", "Mod");
-  AlfAssert(mClassMod, "Failed to find class 'Diabas.Mod' in 'diabas.dll'");
-
-  mFieldModId = mono_class_get_field_from_name(mClassMod, "mId");
-  AlfAssert(mFieldModId, "Failed to find field 'mId' in class 'Mod'");
-  mFieldModName = mono_class_get_field_from_name(mClassMod, "mName");
-  AlfAssert(mFieldModName, "Failed to find field 'mName' in class 'Mod'");
-
   // Util
-  mClassUtil = mono_class_from_name(mCoreImage, "Diabas", "Util");
-  AlfAssert(mClassUtil, "Failed to find class 'Diabas.Util' in 'diabas.dll'");
-
-  mMethodGetAttributes =
-    mono_class_get_method_from_name(mClassUtil, "GetAttributes", 1);
-  AlfAssert(mMethodGetAttributes,
+  mUtil.classUtil = mono_class_from_name(mCoreImage, "Diabas", "Util");
+  AlfAssert(mUtil.classUtil,
+            "Failed to find class 'Diabas.Util' in 'diabas.dll'");
+  mUtil.methodGetAttributes =
+    mono_class_get_method_from_name(mUtil.classUtil, "GetAttributes", 1);
+  AlfAssert(mUtil.classUtil,
             "Failed to find method 'GetAttributes' in class 'Util'");
+
+  // Mod
+  mMod.classMod = mono_class_from_name(mCoreImage, "Diabas.Attribute", "Mod");
+  AlfAssert(mMod.classMod,
+            "Failed to find class 'Diabas.Attribute.Mod' in 'diabas.dll'");
+  mMod.classModId =
+    mono_class_from_name(mCoreImage, "Diabas.Attribute", "ModId");
+  AlfAssert(mMod.classModId,
+            "Failed to find class 'Diabas.Attribute.ModId' in 'diabas.dll'");
+  mMod.classModName =
+    mono_class_from_name(mCoreImage, "Diabas.Attribute", "ModName");
+  AlfAssert(mMod.classModName,
+            "Failed to find class 'Diabas.Attribute.ModName' in 'diabas.dll'");
+  mMod.classModRegisterTiles =
+    mono_class_from_name(mCoreImage, "Diabas.Attribute", "ModRegisterTile");
+  AlfAssert(
+    mMod.classModRegisterTiles,
+    "Failed to find class 'Diabas.Attribute.ModRegisterTiles' in 'diabas.dll'");
+  mMod.classModUpdate =
+    mono_class_from_name(mCoreImage, "Diabas.Attribute", "ModUpdate");
+  AlfAssert(
+    mMod.classModUpdate,
+    "Failed to find class 'Diabas.Attribute.ModUpdate' in 'diabas.dll'");
+
+  // Tile
 }
 
 // -------------------------------------------------------------------------- //
@@ -94,6 +112,20 @@ MonoContext::ListClassesInAssembly(MonoAssembly* assembly)
 
 // -------------------------------------------------------------------------- //
 
+std::vector<MonoClassField*>
+MonoContext::ListFieldsInClass(MonoClass* cls)
+{
+  std::vector<MonoClassField*> list;
+  void* iter;
+  for (int i = 0; i < mono_class_num_fields(cls); i++) {
+    MonoClassField* field = mono_class_get_fields(cls, &iter);
+    list.push_back(field);
+  }
+  return list;
+}
+
+// -------------------------------------------------------------------------- //
+
 std::vector<MonoObject*>
 MonoContext::ListAttributes(MonoType* type)
 {
@@ -103,7 +135,7 @@ MonoContext::ListAttributes(MonoType* type)
   void* params[1];
   params[0] = reflType;
   MonoObject* result = mono_runtime_invoke(
-    Instance().mMethodGetAttributes, nullptr, params, nullptr);
+    Instance().mUtil.methodGetAttributes, nullptr, params, nullptr);
   MonoArray* attributeArray = (MonoArray*)result;
   for (u64 i = 0; i < mono_array_length(attributeArray); i++) {
     MonoObject* object = mono_array_get(attributeArray, MonoObject*, i);
@@ -116,19 +148,30 @@ MonoContext::ListAttributes(MonoType* type)
 // -------------------------------------------------------------------------- //
 
 MonoObject*
-MonoContext::ModAttribute(MonoClass* cls)
+MonoContext::GetClassAttribute(MonoClass* cls, MonoClass* attributeClass)
 {
   // Retrieve all attributes
   MonoType* type = mono_class_get_type(cls);
   std::vector<MonoObject*> attributes = ListAttributes(type);
 
-  // Check if any attribute is a mod
+  // Return any matching attribute object
   for (MonoObject* attribute : attributes) {
-    MonoClass* attributeClass = mono_object_get_class(attribute);
-    if (attributeClass == Instance().mClassMod) {
+    MonoClass* _attributeClass = mono_object_get_class(attribute);
+    if (_attributeClass == attributeClass) {
       return attribute;
     }
   }
+  return nullptr;
+}
+
+// -------------------------------------------------------------------------- //
+
+MonoObject*
+MonoContext::GetFieldAttribute(MonoClass* cls,
+                               MonoClassField* field,
+                               MonoClass* attributeClass)
+{
+
   return nullptr;
 }
 
--- source/game/mod/mono_context.hpp
+++ source/game/mod/mono_context.hpp
@@ -26,17 +26,35 @@ private:
   /** Core assembly image **/
   MonoImage* mCoreImage;
 
-  /** 'Mod' class from core assembly **/
-  MonoClass* mClassMod;
-  /** 'mId' field of 'Mod' class **/
-  MonoClassField* mFieldModId;
-  /** 'mName' field of 'Mod' class **/
-  MonoClassField* mFieldModName;
+  /** 'Util' related objects **/
+  struct
+  {
+    /** 'Util' class from core assembly  **/
+    MonoClass* classUtil;
+    /** 'GetAttributes' method from 'Util' class **/
+    MonoMethod* methodGetAttributes;
+  } mUtil;
 
-  /** 'Util' class from core assembly  **/
-  MonoClass* mClassUtil;
-  /** 'GetAttributes' method from 'Util' class **/
-  MonoMethod* mMethodGetAttributes;
+  /** 'Mod' related objects **/
+  struct
+  {
+    /** 'Mod' class from core assembly **/
+    MonoClass* classMod;
+    /** 'ModId' class from core assembly **/
+    MonoClass* classModId;
+    /** 'ModName' class from core assembly **/
+    MonoClass* classModName;
+    /** 'ModRegisterTiles' class from core assembly **/
+    MonoClass* classModRegisterTiles;
+    /** 'Mod' class from core assembly **/
+    MonoClass* classModUpdate;
+  } mMod;
+
+  /** 'Tile' and 'TileRegistry' related objects **/
+  struct
+  {
+
+  } mTile;
 
 public:
   /** Destruct context **/
@@ -53,18 +71,31 @@ public:
   /** Returns a list of all classes in an assembly **/
   static std::vector<MonoClass*> ListClassesInAssembly(MonoAssembly* assembly);
 
+  /** Returns a list of fields in a class **/
+  static std::vector<MonoClassField*> ListFieldsInClass(MonoClass* cls);
+
   /** Returns a list of all attributes for a type **/
   static std::vector<MonoObject*> ListAttributes(MonoType* type);
 
-  /** Returns the mod attribute of a class. Or null if the class does not have
-   * one **/
-  static MonoObject* ModAttribute(MonoClass* cls);
+  /** Returns the attribute of a class. Or null if the class does not have
+   * that attribute **/
+  static MonoObject* GetClassAttribute(MonoClass* cls,
+                                       MonoClass* attributeClass);
 
-  /** Returns the field 'Diabas.Mod.mId' **/
-  static MonoClassField* GetFieldModId() { return Instance().mFieldModId; }
+  /** Returns the attribute of a field. Or null if the field does not have
+   * that attribute **/
+  static MonoObject* GetFieldAttribute(MonoClass* cls,
+                                       MonoClassField* field,
+                                       MonoClass* attributeClass);
 
-  /** Returns the field 'Diabas.Mod.mName' **/
-  static MonoClassField* GetFieldModName() { return Instance().mFieldModName; }
+  /** Returns the 'Mod' class **/
+  static MonoClass* GetModClass() { return Instance().mMod.classMod; }
+
+  /** Returns the 'ModId' class **/
+  static MonoClass* GetModIdClass() { return Instance().mMod.classModId; }
+
+  /** Returns the 'ModName' class **/
+  static MonoClass* GetModNameClass() { return Instance().mMod.classModName; }
 
 private:
   /** Returns the singleton instance **/
